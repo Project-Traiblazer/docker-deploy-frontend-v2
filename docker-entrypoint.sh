865408708800cd51cbfb475568c194fcad16de2f
HrMSTrailblazer <aungkhaingphyo_1@cmu.ac.th>
2026-03-05 22:48:21 +0700

    Fix: docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 set -e
 
-VITE_GRAPHQL_URI="${VITE_GRAPHQL_URI:-http://43.208.224.38:8082/graphql}"
-VITE_SERVER_URI="${VITE_SERVER_URI:http://43.208.224.38:8082}"
+# 1. Fixed the missing '-' in VITE_SERVER_URI
+VITE_GRAPHQL_URI="${VITE_GRAPHQL_URI:-http://your-actual-ip:8082/graphql}"
+VITE_SERVER_URI="${VITE_SERVER_URI:-http://your-actual-ip:8082}"
 
-find /usr/share/nginx/html/assets -name '*.js' -exec sed -i
-"s|__VITE_GRAPHQL_URI_PLACEHOLDER__|${VITE_GRAPHQL_URI}|g" {} + find /usr/share/nginx/html/assets -name '*.js' -exec sed -i
-"s|__VITE_SERVER_URI_PLACEHOLDER__|${VITE_SERVER_URI}|g" {} + echo "Configured VITE_GRAPHQL_URI=${VITE_GRAPHQL_URI}" echo "Configured VITE_GRAPHQL_URI=${VITE_GRAPHQL_URI}" exec nginx -g 'daemon off;'
+# 2. Fixed the 'find' syntax and separated the commands properly
+find /usr/share/nginx/html/assets -name '*.js' -exec sed -i "s|__VITE_GRAPHQL_URI_PLACEHOLDER__|${VITE_GRAPHQL_URI}|g" {} +
+find /usr/share/nginx/html/assets -name '*.js' -exec sed -i "s|__VITE_SERVER_URI_PLACEHOLDER__|${VITE_SERVER_URI}|g" {} +
+
+echo "Configured VITE_GRAPHQL_URI=${VITE_GRAPHQL_URI}"
+echo "Configured VITE_SERVER_URI=${VITE_SERVER_URI}"
+
+exec nginx -g 'daemon off;'
